568532fc4eee9ec39f0022a62154613fe1d7fb64
SetiZ <funkysetiz@gmail.com>
2016-06-08 23:51:14 +0200

    animation and resizing

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function showRight() {
     let elem = document.querySelector(".right-pane")
-    elem.style.display = ''
+    elem.style.right = '0'
 }
 
 function hideRight() {
     let elem = document.querySelector(".right-pane")
-    elem.style.display = 'none'
+    elem.style.right = '-350px'
 }
