a0798cdc87d04a4f983ab3e75c6fcec01de6b12e
SetiZ <funkysetiz@gmail.com>
2016-06-08 22:24:31 +0200

    exemple1 finished

---
+++ script.js
@@ -0,0 +1,9 @@
+function showRight() {
+    let elem = document.querySelector(".right-pane")
+    elem.style.display = ''
+}
+
+function hideRight() {
+    let elem = document.querySelector(".right-pane")
+    elem.style.display = 'none'
+}
