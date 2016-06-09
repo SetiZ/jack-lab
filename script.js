521eca03d4a2e9d59750d00b0c292107955b62a9
SetiZ <funkysetiz@gmail.com>
2016-06-10 00:37:35 +0200

    all animations

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ function hideRight() {
     let elem = document.querySelector(".right-pane")
     elem.style.right = '-350px'
 }
+
+function viewImage() {
+    let elem = document.querySelector(".big-image")
+    elem.style.top = "0"
+}
+
+function hideImage() {
+    let elem = document.querySelector(".big-image")
+    elem.style.top = "-100%"
+}
