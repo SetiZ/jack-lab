46afc0147dc1d0758aa7c9c75e3600de9191f528
SetiZ <funkysetiz@gmail.com>
2016-06-12 10:06:44 +0200

    let to var

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function showRight() {
-    let elem = document.querySelector(".right-pane")
+    var elem = document.querySelector(".right-pane")
     elem.style.right = '0'
 }
 
 function hideRight() {
-    let elem = document.querySelector(".right-pane")
+    var elem = document.querySelector(".right-pane")
     elem.style.right = '-350px'
 }
 
 function viewImage() {
-    let elem = document.querySelector(".big-image")
+    var elem = document.querySelector(".big-image")
     elem.style.top = "0"
 }
 
 function hideImage() {
-    let elem = document.querySelector(".big-image")
+    var elem = document.querySelector(".big-image")
     elem.style.top = "-100%"
 }
