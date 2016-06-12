28954fa201ea3b884b18600498b308492f6157b5
SetiZ <funkysetiz@gmail.com>
2016-06-12 13:02:17 +0200

    new animations

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 function showRight() {
     var elem = document.querySelector(".right-pane")
-    elem.style.right = '0'
+    var dot = document.querySelector(".red")
+    var margin = document.querySelector(".right-middle")
+    elem.style.width = '350px'
+    margin.style.marginRight = '350px';
+    setTimeout( function() {
+        if (dot.classList) {
+            dot.classList.remove("red-scale")
+        } 
+    }, 500)
+    
+
 }
 
 function hideRight() {
     var elem = document.querySelector(".right-pane")
-    elem.style.right = '-350px'
+    var dot = document.querySelector(".red")
+    var margin = document.querySelector(".right-middle")
+    dot.className += " red-scale"
+    setTimeout( function() {
+    elem.style.width = '0'
+    margin.style.marginRight = '0';
+    }, 500)
 }
 
 function viewImage() {
@@ -17,3 +33,4 @@ function hideImage() {
     var elem = document.querySelector(".big-image")
     elem.style.top = "-100%"
 }
+  
